0937037457b56d8b2b89c08f30d4773055543667
Iván Montes <drslump@pollinimini.net>
2019-10-25 22:46:32 +0200

    tests passing

--- test/test.js
+++ test/test.js
@@ -6,7 +6,6 @@ const execa = require('execa');
 const tempy = require('tempy');
 const binCheck = require('bin-check');
 const binBuild = require('bin-build');
-const compareSize = require('compare-size');
 const readChunk = require('read-chunk');
 const isWebp = require('is-webp');
 const bin = require('..');
@@ -26,19 +25,19 @@ test('return path to binary and verify that it is working', async t => {
 	t.true(await binCheck(bin, ['-help']));
 });
 
-test('convert a PNG to WebP', async t => {
+test('convert PNGs to WebP', async t => {
 	const tmp = tempy.directory();
-	const src = path.join(__dirname, 'fixtures/test.png');
 	const dest = path.join(tmp, 'test.webp');
-	const args = [
-		src,
-		'-o',
-		dest
-	];
+	const args = [];
+
+	for (let i = 0; i < 29; i++) {
+		const suffix = i < 10 ? '0' + i : i;
+		args.push(path.join(__dirname, 'fixtures/frame' + suffix + '.png'));
+	}
+
+	args.push('-o', dest);
 
 	await execa(bin, args);
-	const res = await compareSize(src, dest);
 
-	t.true(res[dest] < res[src]);
 	t.true(isWebp(await readChunk(dest, 0, 12)));
 });
